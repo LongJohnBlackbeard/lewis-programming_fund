81d4848b9d851ccc1673f72b29bec74db7fe865f
Daniel Tujo <danieltujo@lewisu.edu>
2021-02-03 11:59:47 -0600

    Initial commit

---
+++ week3exampleGPA/main.py
@@ -0,0 +1,14 @@
+# Daniel Tujo
+# CPSC 21000 002
+# This program awatds graduation honors
+
+gpa = float(input("Enter you gpa: "))
+if gpa >=3.9:
+    print("Highest Honors")
+elif gpa >= 3.75:
+    print("High Honors")
+elif gpa >= 3.5:
+    print("Honors")
+else:
+    print("No Honors")
+
